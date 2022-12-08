f11b485754b08e144657f36c93a47c6c5aaf887b
hamadtarabeih <118848249+hamadtarabeih@users.noreply.github.com>
2022-12-08 16:19:30 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+#include "my_mat.h"
+#include "my_mat.c"
+#define N 10
+
+int main(){
+int i, j;
+char x='1';
+
+while (x !='D'){
+scanf("%c", &x);
+if(x=='A'){
+GetArr();
+ShortestPaths();
+}
+
+if(x=='B'){
+scanf("%d",&i);
+scanf("%d", &j);
+if(mat[i][j] >0){
+printf("True\n");
+}
+else{
+printf("False\n");
+}
+}
+
+if(x=='C'){
+scanf("%d",&i);
+scanf("%d", &j);
+if(mat[i][j]==0){
+printf("-1\n");
+}
+else{
+printf("%d\n", mat[i][j]);
+}
+}
+}
+}
+
---
+++ my_mat.h
@@ -0,0 +1,12 @@
+#ifndef N 
+#define N 10
+#endif
+int mat[N][N];
+//A function that receives a matrix from the user:
+void GetArr();
+
+//returns the minnimum of 2 integers:
+int MinValue(int a, int b);
+
+//the algorithm:
+void ShortestPaths();
